452be6dda0e7d1b7b8c46f2ab9c5c70d600574c9
Wojciech Maziarz <whitem200@gmail.com>
2024-05-29 18:59:24 +0200

    draft test

--- tests/tests.py
+++ tests/tests.py
@@ -269,6 +269,11 @@ class SearchTests(JiraTestCase):
         self.assertFalse(hasattr(issues[0].fields, "reporter"))
         self.assertFalse(hasattr(issues[0].fields, "progress"))
 
+    def test_find_issue_field_limiting(self):
+        print(self.jira._fields_cache)
+        assert [] == self.jira._fields_cache
+        assert False
+
     def test_search_issues_expand(self):
         issues = self.jira.search_issues(f"key={self.issue}", expand="changelog")
         issues = cast(ResultList[Issue], issues)
